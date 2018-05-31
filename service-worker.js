ce2ed0cc68d4cda69f8a8bac022f9479a3e95cef
UnknwonHero <unknownpostie@gmail.com>
2018-05-31 06:48:36 +0300

    sync

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+self.addEventListener('push', function (event) {
+    console.log(event);
+    event.waitUntil(self.registration.showNotification('ServiceWorker Cookbook', {
+        body: 'Push Notification Subscription Management'
+    }));
+});
+
+self.addEventListener('pushsubscriptionchange', function (event) {
+    var url = 'http://6764-cloud.hosted-by-virtualdc.ru/subscriber';
+    var sourcePublicKey = 'e6bf5c49-7c90-49f6-a87c-17ef76dcd9b6';
+
+    event.waitUntil(
+        self.registration.pushManager.subscribe({userVisibleOnly: true})
+            .then(function (subscription) {
+                console.log('Subscribed after expiration', subscription.endpoint);
+                return fetch(url, {
+                    method: 'post',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        endpoint: subscription.endpoint,
+                        data: subscription,
+                        sourcePublicKey: sourcePublicKey,
+                    })
+                });
+            })
+    );
+});
