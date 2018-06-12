4bfca5540e64808683d85b4e2be5019b0a745ee6
UnknwonHero <unknownpostie@gmail.com>
2018-06-12 11:45:22 +0300

    sync

--- smi-sw.js
+++ smi-sw.js
@@ -9,37 +9,27 @@ self.addEventListener("push", function (event) {
     var icon = data.icon;
     var url = data.url;
 
-    var notification = new self.Notification(title, {
+    e.waitUntil(self.registration.showNotification(title, {
         body: message,
         icon: icon,
-        url: url,
-    });
+    }))
 
-    notification.addEventListener('click', function() {
-        if (clients.openWindow) {
-            clients.openWindow(url);
-        }
-    });
-
-    // e.waitUntil(self.registration.showNotification(title, {
-    //     body: 'Buzz! Buzz!',
-    //     icon: '../images/touch/chrome-touch-icon-192x192.png',
-    //     vibrate: [200, 100, 200, 100, 200, 100, 200],
-    //     tag: 'vibration-sample'
-    // }))
 });
-//
-// self.addEventListener("pushsubscriptionchange", function (e) {
-//     e.waitUntil(self.registration.pushManager.subscribe({userVisibleOnly: !0}).then(function (e) {
-//         window.smiPush.start();
-//     }))
-// });
 
 self.addEventListener('notificationclick', function (event) {
 
     event.notification.close();
     e.waitUntil(self.registration.pushManager.getSubscription().then(function (data) {
         console.log(data);
+        console.log(event);
+
         clients.openWindow('' + 'click?url=' + event.notification.data.url + '&sourceKey=' + +'&endPoint' + data.endpoint)
     }));
+});
+
+self.addEventListener("pushsubscriptionchange", function (e) {
+    console.log(e);
+    e.waitUntil(self.registration.pushManager.subscribe({userVisibleOnly: !0}).then(function (e) {
+        window.smiPush.start();
+    }))
 });
